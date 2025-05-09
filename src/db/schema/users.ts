07f2132302ac1b8d26094a164cd8c8db26862f94
mtguerson <mtguerson@gmail.com>
2025-05-09 11:44:32 -0400

    feat: add create-user route

--- src/db/schema/users.ts
+++ src/db/schema/users.ts
@@ -1,7 +1,10 @@
-import { pgTable, serial, text } from "drizzle-orm/pg-core";
+import { pgTable, text } from "drizzle-orm/pg-core";
+import { createId } from "@paralleldrive/cuid2";
 
-export const user = pgTable("users", {
-  id: serial("id").primaryKey(),
+export const users = pgTable("users", {
+  id: text("id")
+    .$defaultFn(() => createId())
+    .primaryKey(),
   name: text("name").notNull(),
   email: text("email").notNull().unique(),
   products: text("products").array(),
---
+++ src/http/routes/create-user.ts
@@ -0,0 +1,29 @@
+import Elysia, { t } from "elysia";
+import { db } from "../../db/connection";
+import { users } from "../../db/schema";
+import { eq } from "drizzle-orm";
+
+export const createUser = new Elysia().post(
+  "/create-user",
+  async ({ body, set }) => {
+    const { name, email, products } = body;
+
+    const user = await db.select().from(users).where(eq(users.email, email));
+
+    if (user.length > 0) {
+      set.status = 400;
+      return "User already exists";
+    }
+
+    await db.insert(users).values({ name, email, products });
+
+    set.status = 204;
+  },
+  {
+    body: t.Object({
+      name: t.String(),
+      email: t.String({ format: "email" }),
+      products: t.Array(t.String()),
+    }),
+  }
+);
